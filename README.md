1ad761bdbb7c1778bdc72fccd7af9017f17e7b7d
FloZone <rod.florian@gmail.com>
2023-11-07 17:56:36 +0100

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# ColoredManga Downloader
+![Logo](logo.png)
+
+A simple script for browsing the ColoredManga.com website and download the content.
+
+It is written in python.
+
+## Installing dependencies
+All mandatory libraries and dependancies are listed in `requirements.txt`.
+```bash
+pip install -r ./requirements.txt
+```
+
+## Command-line help
+```
+usage: python coloredmanga_downloader.py [-h]
+
+Browse the ColoredManga.com website and download the content.
+
+optional arguments:
+  -h, --help            show this help message and exit
+```
+
+## Other commands
+* `python setup.py lint` : for linting the script file
+* `python setup.py fmt` : for formatting the script file
---
+++ setup.py
@@ -0,0 +1,77 @@
+import distutils.cmd
+import distutils.log
+import os
+import subprocess
+
+from setuptools import find_packages, setup
+
+INPUT_FILE = "coloredmanga_downloader.py"
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+class LintCommand(distutils.cmd.Command):
+    """Lint with flake8."""
+
+    description = "run flake8 on source files"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self) -> bool:
+        self.announce("Linting...", level=distutils.log.WARN)
+
+        self.announce("flake8 pass...", level=distutils.log.WARN)
+        return subprocess.run(["flake8", INPUT_FILE]).returncode == 0
+
+
+class FormatCommand(distutils.cmd.Command):
+    """Format with black."""
+
+    description = "Run isort and black on source files"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self) -> bool:
+        self.announce("Formatting...", level=distutils.log.WARN)
+
+        self.announce("isort pass...", level=distutils.log.WARN)
+        if subprocess.run(["isort", "-rc", "--atomic", INPUT_FILE]).returncode != 0:
+            return False
+
+        self.announce("black pass...", level=distutils.log.WARN)
+        return subprocess.run(
+            [
+                "black",
+                "--target-version",
+                "py38",
+                "-l",
+                "120",
+                INPUT_FILE,
+            ],
+        ).returncode == 0
+
+
+setup(
+    name="",
+    version="",
+    author="FloZone",
+    description="",
+    long_description=read("README.md"),
+    packages=find_packages(),
+    cmdclass={
+        "lint": LintCommand,
+        "fmt": FormatCommand,
+    },
+)
